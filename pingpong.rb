1ed108acb285b30134d491dc2b9b21c689029285
Joe Chrysler <mail@joechrysler.com>
2012-06-11 16:13:16 -0300

    A small smidgen of code gardening. You're welcome, future me.

--- pingpong.rb
+++ pingpong.rb
@@ -4,8 +4,8 @@ require "redis"
 origin = Redis.new(host: '127.0.0.1', port: 6379)
 destination = Redis.new(host: '127.0.0.1', port: 6380)
 
-
 def migrate(from, to)
+  # migrate keys from a redis connection to another one
   keys = from.keys '*'
   from.pipelined do
     keys.each do |key|
@@ -15,24 +15,29 @@ def migrate(from, to)
 end
 
 def message(o, d)
+  # this is a lazily imagined method
   puts "key counts %3d:%3d" % [o.dbsize, d.dbsize]
 end
 
 
+# flush any old stuff
 origin.flushall
 destination.flushall
 
+# generate some new stuff
 origin.pipelined do
   500.times do |i|
     origin.set(i, "value#{i}")
   end
 end
 
+
 loop do
+  # bounce data between a couple redis connections
   message(origin, destination)
   migrate(origin, destination)
   message(origin, destination)
   migrate(destination, origin)
   puts
-  sleep 0.5
+  sleep 1.0
 end
